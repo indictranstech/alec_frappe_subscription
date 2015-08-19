175924808a40fd65e9c131368636959004924039
Makarand <mbauskar@gmail.com>
2015-08-19 11:25:52 +0530

    select field trigger on packing slip

--- frappe_subscription/frappe_subscription/packing_slip.js
+++ frappe_subscription/frappe_subscription/packing_slip.js
@@ -2,3 +2,14 @@ cur_frm.cscript.after_cancel = function(doc,cdt,cdn){
     frappe.msgprint("Packing Slip is Cancelled and Deleted")
     frappe.set_route('List', "Packing Slip");
 }
+
+cur_frm.cscript.track_status = function(doc,cdt,cdn){
+    if(doc.track_status == "Auto"){
+        // cur_frm.set_df_property("tracking_id", "read_only", 1);
+        cur_frm.set_df_property("tracking_status", "read_only", 1);
+    }
+    else{
+        // cur_frm.set_df_property("tracking_id", "read_only", 0);
+        cur_frm.set_df_property("tracking_status", "read_only", 0);
+    }
+}
